8fabc14f8fc90bdf96369340618999c5691f6157
lmarchet <leo.marchetti@hotmail.ca>
2018-06-06 15:15:40 -0400

    final version moving FB code to java script file and cleaned index file

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// Facebook SDK for javascript BEGIN HERE -->
-
 function undisableTxt() {
     document.getElementById('sname').disabled = false;
     document.getElementById('address').disabled = false;
@@ -17,9 +15,7 @@ function undisableTxt() {
     function statusChangeCallback(response) {
     console.log('statusChangeCallback');
     console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
+    // The response object is returned with a status field that lets the app know the current login status of the person. Full docs on the response object can be found in the documentation
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
@@ -32,9 +28,7 @@ function undisableTxt() {
     }
   }
 
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
+  // This function is called when someone finishes with the Login Button.  See the onlogin handler attached to it in the sample code below.
   function checkLoginState() {
     FB.getLoginStatus(function(response) {
       statusChangeCallback(response);
@@ -77,8 +71,7 @@ function undisableTxt() {
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
+  // Here we run a very simple test of the Graph API after login is successful.  See statusChangeCallback() for when this call is made.
   function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
@@ -88,7 +81,6 @@ function undisableTxt() {
     });
   }
   
- 
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBomPRLZ-HhBbs_p8_3o6VcCFExmu0K7F8",
